ccd5f32a9baab05eba725128cbf9710138c02754
Chifflo <chifflo.rasch@gmail.com>
2017-03-30 18:52:07 +0200

    Setup from last version update

---
+++ Source/Mechopia/Mr_Mushy.h
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Character.h"
+#include "Mr_Mushy.generated.h"
+
+UCLASS()
+class MECHOPIA_API AMr_Mushy : public ACharacter
+{
+	GENERATED_BODY()
+
+		UPROPERTY(EditAnywhere, Category = "Spawning")
+		TSubclassOf<class AMushy_HBox> Mushy_HBox_BP;
+
+
+public:
+	// Sets default values for this character's properties
+	AMr_Mushy();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void Tick( float DeltaSeconds ) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	
+	FVector ToPlayer;
+	FVector Direction;
+	FRotator Rotation;
+	float Counter = 1.f;
+	float Health = 3.f;
+	void Dash();
+	void Move();
+	void Attack();
+	void OnHit();
+	bool Close = false;
+	bool Attacking = false;
+	FTimerHandle DashTimerHandle;
+};
